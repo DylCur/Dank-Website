b1d880d28af60bf7a7ef69742593e7f522942018
DylCur <dylancurry001@gmail.com>
2025-05-07 21:14:46 +0100

    Damn son whered you find this

--- app/js/dankmemes.js
+++ app/js/dankmemes.js
@@ -7,9 +7,12 @@ function GenerateDankMemes(){
         "When the wife won't shutup",
         "Her: Hes probably out cheating || Him: Smoking dank weed with da boiz",
         "Only boys will understand",
-        "Women: Out signals are clear why don't hey get me? Their signals: ",
         "Gay men be like: I think... i'll get the penis",
         "That feeling when the wife is at work so you can watch football and drink beer",
+        "Gay men when buying ducks: ermm... i think ill buy the dick, i mean dick, i mean dick, sorry i mean dick, i mean dick, sorry i mean dick",
+        "All my wife does is shop 😂",
+        "I can haz cheezburger 😂",
+        "Hey dawg, can i get some ice cream? 1000 upvotes for part 2"
     ]
 
     const num = Math.floor(Math.random() * dankMemes.length);
