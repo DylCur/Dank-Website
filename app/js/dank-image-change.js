e0f985bd29d665a88fd7dcf79acd6556cdca5d1f
Dylan.Curry (12D) <Dylan.Curry@utcmediacityuk.org.uk>
2025-05-08 14:58:33 +0100

    Wa

--- app/js/dank-image-change.js
+++ app/js/dank-image-change.js
@@ -1,4 +1,25 @@
 function dank(){
+
+    // document.addEventListener("keydown", function(){
+    //     document.querySelectorAll('.dank-image').forEach(function(img) {
+    //         const dankImages = [
+    //             "https://www.dictionary.com/e/wp-content/uploads/2018/03/dank-meme.jpg",
+    //             "https://i.ytimg.com/vi/BcjfZxh5Stg/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAkYX1apUQ3KqJzMl_EXE9V2Zu3hg",
+    //             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMHzQpvYkaGcqtg9VDBMP9y8dFWnchG5Z2qA&s",
+    //             "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1454038999i/28797157.jpg"
+    //         ]
+            
+    //         let image = img.src;
+    //         while(image == img.src){
+    //             image = dankImages[Math.floor(Math.random() * dankImages.length)];
+    //         }
+
+    //         img.src = image;
+        
+    //         console.log(img.src);
+    //     });
+    // });
+
     document.querySelectorAll('.dank-image').forEach(function(img) {
         img.addEventListener('click', function () {
             const dankImages = [
